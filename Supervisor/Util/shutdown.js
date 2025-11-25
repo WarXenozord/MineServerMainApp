766fc728f9f820f8c514e2536ea5d3327ae788c9
Juan Libonatti <63894831+WarXenozord@users.noreply.github.com>
2025-11-24 22:55:09 -0300

    fix: instance shutdown id

--- Supervisor/Util/shutdown.js
+++ Supervisor/Util/shutdown.js
@@ -4,6 +4,7 @@ dotenv.config({ path: process.env.NODE_ENV === "production" ? "/etc/msu/.env" : 
 import { EC2Client, StopInstancesCommand } from "@aws-sdk/client-ec2";
 
 const client = new EC2Client({ region: process.env.AWS_REGION });
+const INSTANCE_ID = process.env.INSTANCE_ID;
 
 export async function turnOff(){
     await client.send(new StopInstancesCommand({
